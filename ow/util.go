0b4682866dce9883fd7b3d3210eb358c1215da8e
Alexandre Boucey <alexandre.boucey@gmail.com>
2017-10-06 20:57:57 +0200

    Added SanitizeValue function

--- ow/util.go
+++ ow/util.go
@@ -3,6 +3,8 @@ package ow
 import (
 	"regexp"
 	"strings"
+	"strconv"
+	"log"
 )
 
 var spaceRegex = regexp.MustCompile(`[-\s]`)
@@ -18,4 +20,80 @@ func SanitizeKey(text string) string {
 	text = strings.Replace(text, "soldier_76", "soldier76", 1)
 
 	return text
+}
+
+func SanitizeValue(text string) float32 {
+	text = strings.ToLower(text)
+
+	longNum := strings.Replace(text, ",", "", -1)
+	if v, err := strconv.Atoi(longNum); err == nil {
+		return float32(v)
+	}
+
+	// value is a percentage
+	if text[len(text)-1:] == "%" {
+		if v, err := strconv.Atoi(text[:len(text)-1]); err == nil {
+			return float32(v) / 100
+		}
+
+		log.Printf("Value is a percentage but failed to decode: %s\n", text)
+
+		return 0
+	}
+
+	if strings.ContainsRune(text, ':') {
+		parts := strings.Split(text, ":")
+		fnToSimpleFloat32 := func(t string) float32 {
+			if v, err := strconv.Atoi(t); err == nil {
+				return float32(v)
+			}
+
+			return 0
+		}
+
+		var h, m, s float32
+		switch len(parts) {
+		case 3:
+			h, m, s = fnToSimpleFloat32(parts[0]), fnToSimpleFloat32(parts[1]), fnToSimpleFloat32(parts[2])
+		case 2:
+			m, s = fnToSimpleFloat32(parts[0]), fnToSimpleFloat32(parts[1])
+		case 1:
+			s = fnToSimpleFloat32(parts[0])
+		}
+
+		return h + ((m + (s / 60)) / 60)
+	}
+
+	if strings.HasSuffix(text, " hours") {
+		if v, err := strconv.Atoi(text[:len(text)-6]); err == nil {
+			return float32(v)
+		}
+
+		log.Printf("Value is an hour but failed to decode: %s\n", text)
+
+		return 0
+	}
+
+	if strings.HasSuffix(text, " minutes") {
+		if v, err := strconv.Atoi(text[:len(text)-6]); err == nil {
+			return float32(v) / 60.0
+		}
+
+		log.Printf("Value is a minute but failed to decode: %s\n", text)
+
+		return 0
+	}
+
+	if strings.HasSuffix(text, " seconds") {
+		if v, err := strconv.Atoi(text[:len(text)-6]); err == nil {
+			return float32(v) / 3600.0
+		}
+
+		log.Printf("Value is a second but failed to decode: %s\n", text)
+
+		return 0
+
+	}
+
+	return 0
 }
